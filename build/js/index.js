e524208ea6c566975e9042670dee2c3f494b0680
unknown <hovsepyandavid0@gmail.com>
2021-08-31 14:14:55 +0400

    da

--- build/js/index.js
+++ build/js/index.js
@@ -1,26 +1,59 @@
-var imagesarray = $(".images_wedding_gallery_item")
-$(".images_wedding_gallery_wrapper").remove()
-var max = 12;
-var imgboxarray = 0;
-var maximg = 0;
-var b = 0;
-$(document).ready(function(){
-  for (var i = 0; i < imagesarray.length; i++) {
-    maximg = imagesarray.length/12
-    if(i<=maximg){
-      $(".imgboxhide").append("<div class='images_wedding_gallery_wrapper'></div>")
-    }
-    imgboxarray = $(".images_wedding_gallery_wrapper");
+var swiper = new Swiper(".main_cont_swiper", {
+  slidesPerView: 'auto',
+  pagination: {
+    el: ".swiper-pagination-main",
+  },
+  loop:true
+});
+var swiper = new Swiper(".catalog_cont_slider", {
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: ".slidenext",
+    prevEl: ".slideprev",
+  },
+  loop:true
+});
+var swiper = new Swiper(".goodtoknow_cont_slide", {
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: ".slidenext2",
+    prevEl: ".slideprev2",
+  },
+  loop:true
+});
+var swiper = new Swiper(".trademarks_cont_slide", {
+  slidesPerView: 'auto',
+  loop:true
+});
+var formitem = $(".forms_cont_box_item")
+$(formitem[0]).show()
+$(".formstabbtn").click(function(){
+  $(formitem).hide()
+  $(formitem[$(this).val()]).show()
+  $(".formstabbtn").removeClass("active")
+  $(this).addClass("active")
+})
+if($(window).width()<694){
+  var swiper = new Swiper(".services_cont_slider", {
+    slidesPerView: 'auto',
+    pagination: {
+      el: ".swiper-pagination-services",
+    },
+    loop:true
+  });
+}
+$(window).resize(function(){
+  if($(window).width()<694){
+    var swiperas = new Swiper(".services_cont_slider", {
+      slidesPerView: 'auto',
+      pagination: {
+        el: ".swiper-pagination-services",
+      },
+      loop:true
+    });
   }
-  for (var i = 0; i < imagesarray.length; i++) {
-    if(i<max){
-      $(imgboxarray[b]).append(imagesarray[i])
-      console.log(i)
-      if(i>=max-1){
-        b+=1;
-        max+=12;
-        console.log("exav")
-      }
-    }
-  }
+})
+$(".forms_cont_box_item_btn").click(function(){
+  $(this).toggleClass("activ");
+  $(this).siblings(".forms_cont_box_item_hide").slideToggle(500)
 })
